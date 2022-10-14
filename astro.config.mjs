7540b6d427dfd738f722787b8caadb851c9d3692
Paul2D <paul.maxineanu@gmail.com>
2022-10-14 12:34:36 +0300

    integrate google analytics code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import partytown from "@astrojs/partytown";
 
 import vue from "@astrojs/vue";
 
@@ -19,11 +20,21 @@ export default defineConfig({
     }
   },
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue()],
+  integrations: [mdx(),
+      sitemap(), 
+      tailwind({
+        config: {
+          applyBaseStyles: false
+        }
+      }), 
+      vue(),
+      partytown({
+        // Adds dataLayer.push as a forwarding-event.
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      })
+  ],
   site: 'https://paul2d.dev',
   // base: '/astro-blog',
 });
